cde5c21d84cd780f907fdde97fd7d3632c6883dc
Cihat Salik <cihatsalik1@gmail.com>
2022-09-26 17:42:09 +0300

    Remove dashboard template

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,178 +1,130 @@
-<!-- 
-	This is the main page of the application, the layout component is used here,
-	and the router-view is passed to it.
-	Layout component is dynamically declared based on the layout for each route,
-	specified in routes list router/index.js .
- -->
-
- <template>
-	<div id="app">
-		<component :is="layout">
-			<router-view />
-		</component>
-	</div>
-</template>
-
 <script>
-
-	export default ({
-		computed: {
-			// Sets components name based on current route's specified layout, defaults to
-			// <layout-default></layout-default> component.
-			layout() {
-				return "layout-" + ( this.$route.meta.layout || "default" ).toLowerCase() ;
-			}
-		},
-	})
-	
-</script>
-
-<style lang="scss">
-</style>
-
-
-<!-- <script>
-import { useAccountStore } from '@/store';
+  import { useAccountStore } from '@/store';
 import { message } from 'ant-design-vue';
 import { mapActions, mapState } from 'pinia';
-
-export default {
-  name: "App",
-  computed: {
-    ...mapState(useAccountStore, ["user"]),
-    selectedKeys() {
-      return [this.$route?.name]
+  export default {
+    name: "App",
+    computed: {
+      ...mapState(useAccountStore, ["user"]),
+      selectedKeys() {
+        return [this.$route?.name]
+      },
     },
-  },
-  methods: {
-    ...mapActions(useAccountStore, ["logout"]),
-    async doLogout() {
-      await this.logout()
-      message.success('Logout successful 🎉✅🎉')
-      this.$router.push("/")
+    methods: {
+      ...mapActions(useAccountStore, ["logout"]),
+      async doLogout() {
+        await this.logout()
+        message.success('Logout successful 🎉✅🎉')
+        this.$router.push("/")
+      },
     },
-  },
-}
-</script>
-<template>
-  <div id="app" :class="$route?.name">
-    <a-layout class="layout" id="components-layout-demo-top" theme="light">
-      <a-layout-header>
-        <a-row type="flex" justify="space-between">
-          <a-col>
-            <div class="logo">
-              <router-link to="/">Boiler Plate</router-link><span>BETA</span>
-            </div>
-          </a-col>
-          <a-col class="navigation">
-            <a-menu v-if="user" theme="light" :selectedKeys="selectedKeys" mode="horizontal"
-              :style="{lineHeight: '64px'}" inlineCollapsed="false" ellipsize={false} class="nav-list">
-              <a-menu-item key="register">
-                <router-link :to="`/profile/${user._id}`" v-if="user.username">
-                  {{ user.username }}
-                </router-link>
-              </a-menu-item>
-              <a-menu-item v-if="user" key="logout" @click="doLogout">Log out</a-menu-item>
-            </a-menu>
-            <a-menu v-else mode="horizontal" class="nav-list">
-              <a-menu-item key="login">
-                <router-link to="/login">Log in</router-link>
-              </a-menu-item>
-              <a-menu-item key="register">
-                <router-link to="/register">Sign up</router-link>
-              </a-menu-item>
-            </a-menu>
-          </a-col>
-        </a-row>
-      </a-layout-header>
-      <a-layout-content>
-        <router-view></router-view>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">©2022 Cihat Salik</a-layout-footer>
-    </a-layout>
-  </div>
-</template>
-
-<style lang="scss">
-* {
-  position: relative;
-}
-
-h1 {
-  font-weight: 700 !important;
-}
-</style>
-
-<style lang="scss">
-html,
-body {
-  background-color: transparent !important;
-}
-
-#app {
-  font-family: "Avenir Next", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell,
-    "Open Sans", "Helvetica Neue", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  font-weight: 500;
-  background-color: #fff;
-
-  .logo {
-    z-index: 1;
-    font-weight: bold;
-
-    a {
-      height: 100%;
-      padding-right: 0.3em;
+  }
+  </script>
+  <template>
+    <div id="app" :class="$route?.name">
+      <a-layout class="layout" id="components-layout-demo-top" theme="light">
+        <a-layout-header>
+          <a-row type="flex" justify="space-between">
+            <a-col>
+              <div class="logo">
+                <router-link to="/">Boiler Plate</router-link><span>BETA</span>
+              </div>
+            </a-col>
+            <a-col class="navigation">
+              <a-menu v-if="user" theme="light" :selectedKeys="selectedKeys" mode="horizontal"
+                :style="{lineHeight: '64px'}" inlineCollapsed="false" ellipsize={false} class="nav-list">
+                <a-menu-item key="register">
+                  <router-link :to="`/profile/${user._id}`" v-if="user.username">
+                    {{ user.username }}
+                  </router-link>
+                </a-menu-item>
+                <a-menu-item v-if="user" key="logout" @click="doLogout">Log out</a-menu-item>
+              </a-menu>
+              <a-menu v-else mode="horizontal" class="nav-list">
+                <a-menu-item key="login">
+                  <router-link to="/login">Log in</router-link>
+                </a-menu-item>
+                <a-menu-item key="register">
+                  <router-link to="/register">Sign up</router-link>
+                </a-menu-item>
+              </a-menu>
+            </a-col>
+          </a-row>
+        </a-layout-header>
+        <a-layout-content>
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">©2022 Cihat Salik</a-layout-footer>
+      </a-layout>
+    </div>
+  </template>
+  <style lang="scss">
+  * {
+    position: relative;
+  }
+  h1 {
+    font-weight: 700 !important;
+  }
+  </style>
+  <style lang="scss">
+  html,
+  body {
+    background-color: transparent !important;
+  }
+  #app {
+    font-family: "Avenir Next", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell,
+      "Open Sans", "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    font-weight: 500;
+    background-color: #fff;
+    .logo {
+      z-index: 1;
+      font-weight: bold;
+      a {
+        height: 100%;
+        padding-right: 0.3em;
+        width: 100%;
+      }
+      span {
+        top: -3px;
+        color: white;
+        background: #1890ff;
+        border-radius: 3px;
+        font-size: 8px;
+        padding: 3px;
+      }
+    }
+    .ant-layout {
+      min-height: 100vh;
+    }
+    .ant-layout-header {
+      background: white;
+      padding: 0 calc(calc(100% - 1088px) / 2);
+    }
+    .ant-layout-content {
+      max-width: 1200px;
+      align-self: center;
+      padding: 4em;
       width: 100%;
+      @media (max-width: 576px) {
+        padding: 1em;
+      }
     }
-
-    span {
-      top: -3px;
-      color: white;
-      background: #1890ff;
-      border-radius: 3px;
-      font-size: 8px;
-      padding: 3px;
-    }
-  }
-
-  .ant-layout {
-    min-height: 100vh;
-  }
-
-  .ant-layout-header {
-    background: white;
-    padding: 0 calc(calc(100% - 1088px) / 2);
-  }
-
-  .ant-layout-content {
-    max-width: 1200px;
-    align-self: center;
-    padding: 4em;
-    width: 100%;
-
-    @media (max-width: 576px) {
-      padding: 1em;
-    }
-  }
-
-  .navigation {
-    flex-grow: 2;
-
-    .nav-list {
-      justify-content: flex-end;
-
-      li {
-        span {
-          display: block;
-          a {
+    .navigation {
+      flex-grow: 2;
+      .nav-list {
+        justify-content: flex-end;
+        li {
+          span {
             display: block;
+            a {
+              display: block;
+            }
           }
         }
       }
     }
   }
-}
-</style> -->
+  </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,160 +1,50 @@
+
+
 import { useAccountStoreWithout } from "@/store/account.store"
 import { createRouter, createWebHistory } from "vue-router"
 
-let routes = [
-	{
-		// will match everything
-    path: "/:pathMatch(.*)*",
-		component: () => import('@/views/404.vue'),
-	},
-	{
-		path: '/',
-		name: 'Home',
-		redirect: '/dashboard',
-	},
-	{
-		path: '/dashboard',
-		name: 'Dashboard',
-		layout: "dashboard",
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "dashboard" */ '@/views/Dashboard.vue'),
-	},
-	{
-		path: '/layout',
-		name: 'Layout',
-		layout: "dashboard",
-		component: () => import('@/views/Layout.vue'),
-	},
-	{
-		path: '/tables',
-		name: 'Tables',
-		layout: "dashboard",
-		component: () => import('@/views/Tables.vue'),
-	},
-	{
-		path: '/billing',
-		name: 'Billing',
-		layout: "dashboard",
-		component: () => import('@/views/Billing.vue'),
-	},
-	{
-		path: '/Profile',
-		name: 'Profile',
-		layout: "dashboard",
-		meta: {
-			layoutClass: 'layout-profile',
-		},
-		component: () => import('@/views/Profile.vue'),
-	},
-	{
-		path: '/sign-in',
-		name: 'Sign-In',
-		component: () => import('@/views/Sign-In.vue'),
-	},
-	{
-		path: '/sign-up',
-		name: 'Sign-Up',
-		meta: {
-			layoutClass: 'layout-sign-up',
-		},
-		component: () => import('@/views/Sign-Up.vue'),
-	},
-]
-
-// Adding layout property from each route to the meta
-// object so it can be accessed later.
-function addLayoutToRoute( route, parentLayout = "default" )
-{
-	route.meta = route.meta || {} ;
-	route.meta.layout = route.layout || parentLayout ;
-	
-	if( route.children )
-	{
-		route.children = route.children.map( ( childRoute ) => addLayoutToRoute( childRoute, route.meta.layout ) ) ;
-	}
-	return route ;
-}
-
-routes = routes.map( ( route ) => addLayoutToRoute( route ) ) ;
-
-// const router = new VueRouter({
-// 	mode: 'hash',
-// 	base: process.env.BASE_URL,
-// 	routes,
-// 	scrollBehavior (to, from, savedPosition) {
-// 		if ( to.hash ) {
-// 			return {
-// 				selector: to.hash,
-// 				behavior: 'smooth',
-// 			}
-// 		}
-// 		return {
-// 			x: 0,
-// 			y: 0,
-// 			behavior: 'smooth',
-// 		}
-// 	}
-// })
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-  scrollBehavior (to, from, savedPosition) {
-		if ( to.hash ) {
-			return {
-				selector: to.hash,
-				behavior: 'smooth',
-			}
-		}
-		return {
-			x: 0,
-			y: 0,
-			behavior: 'smooth',
-		}
-	}
-    
-  //   [
-  //   {
-  //     path: "/",
-  //     name: "home-view",
-  //     component: import("@/views/home-view.vue"),
-  //   },
-  //   {
-  //     path: "/register",
-  //     name: "register-view",
-  //     component: import("@/views/Auth/register-view.vue"),
-  //   },
-  //   {
-  //     path: "/login",
-  //     name: "login-view",
-  //     component: () => import("@/views/Auth/login-view.vue"),
-  //   },
-  //   {
-  //     path: "/dashboard",
-  //     name: "dashboard-view",
-  //     component: import("@/views/dashboard-view.vue"),
-  //   },
-  //   {
-  //     path: "/profile/:id",
-  //     name: "profile-view",
-  //     component: import("@/views/profile-view.vue"),
-  //   },
-  //   {
-  //     path: "/:pathMatch(.*)*",
-  //     component: import("@/views/not-found-view.vue"),
-  //   },
-  // ],
+  routes: [
+    {
+      path: "/",
+      name: "home-view",
+      component: import("../views/home-view.vue"),
+    },
+    {
+      path: "/register",
+      name: "register-view",
+      component: import("../views/Auth/register-view.vue"),
+    },
+    {
+      path: "/login",
+      name: "login-view",
+      component: () => import("../views/Auth/login-view.vue"),
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard-view",
+      component: import("../views/dashboard-view.vue"),
+    },
+    {
+      path: "/profile/:id",
+      name: "profile-view",
+      component: import("../views/profile-view.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      component: import("../views/not-found-view.vue"),
+    },
+  ],
 })
 
-// router.beforeEach(async to => {
-//   const publicPages = ["/login", "/register"]
-//   const authRequired = !publicPages.includes(to.path)
-//   const accountStore = useAccountStoreWithout()
+router.beforeEach(async to => {
+  const publicPages = ["/login", "/register"]
+  const authRequired = !publicPages.includes(to.path)
+  const accountStore = useAccountStoreWithout()
 
-//   if (authRequired && !accountStore.user) return router.push("/login")
-//   else if (!authRequired && accountStore.user) return router.push("/dashboard")
-// })
+  if (authRequired && !accountStore.user) return router.push("/login")
+  else if (!authRequired && accountStore.user) return router.push("/dashboard")
+})
 
 export { router }
